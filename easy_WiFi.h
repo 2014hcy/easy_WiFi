e64bf7b4536c72cd109aa3daaa015f36f93d5daf
2014hcy <602260999@qq.com>
2026-02-18 17:36:59 +0800

    Modify connect function to accept OLED address
    Updated connect function to include OLED address parameter.

--- easy_WiFi.h
+++ easy_WiFi.h
@@ -1,13 +1,15 @@
 #ifndef EASY_WIFI_H
 #define EASY_WIFI_H
 
+#include "stdint.h"
+
 enum Country {
   ZH,
   UK,
   US
 };
 
-void connect(const char* ssid, const char* password);
+void connect(const char* ssid, const char* password, uint8_t oled_addr);
 void init_time(Country country);
 void write_time();
 
